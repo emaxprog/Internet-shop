0b733e057caa36d145e662adf4d162d8f095a717
alexandr <emaxprog@gmail.com>
2016-07-28 12:01:34 +0300

    Add files

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Интернет-магазин</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="styles/css/styles.css">
+</head>
+<body>
+<div class="wrapper-header">
+    <div class="mid">
+        <?php include "include/header.html"?>
+    </div>
+</div>
+<div class="wrapper-menu">
+    <div class="mid">
+
+    </div>
+</div>
+<div class="wrapper-content">
+    <div class="mid">
+
+    </div>
+</div>
+<div class="wrapper-footer">
+    <div class="mid">
+
+    </div>
+</div>
+</body>
+</html>
